ac4fd0ae894ac4a771e5fbfd0364a3787f16cd0d
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-01-29 09:58:20 -0300

    Aula 2 - React, State e Form

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,84 @@
-import MainGrid from '../src/components/MainGrid'
-import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AluraCommons'
-import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations'
+import React, { useState } from 'react';
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import styled from 'styled-components';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AluraCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 const ProfileSidebar = (props) => {
-  console.log(props)
   return (
-    <Box>
-      <img src={`https://github.com/${props.gitUser}.png`} style={{ borderRadius: '8px' }} />
+    <Box as="aside">
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
+const GroupRenders = ({ array, groupName }) => {
+  return (
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {groupName} ({array.length})
+      </h2>
+
+      <ul>
+        {array.slice(0, 6).map(item => {
+          return (
+            <li key={item.id}>
+              <a href={item.link ? item.link : 'https://picsum.photos/'} target="_blank">
+                <img src={item.image ? item.image : `https://github.com/${item.name}.png`} />
+                <span>{item.name}</span>
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </ProfileRelationsBoxWrapper>
+  )
+}
+
 export default function Home() {
-  const gitUser = 'Jorge-Neto';
+  const githubUser = 'Jorge-Neto';
+  const [comunidades, setComunidades] = useState([{
+    id: 23242342342342342343454,
+    name: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
   const pessoasFavoritas = [
-    'omariosouto',
-    'peas',
-    'juunegreiros',
-    'rafaballerini',
-    'marcobrunodev',
-    'felipefialho'
-  ]
+    { id: 123, name: 'omariosouto', link: 'https://www.youtube.com/c/DevSoutinho' },
+    { id: 234, name: 'peas', link: 'https://cursos.alura.com.br/user/paulo-silveira' },
+    { id: 345, name: 'juunegreiros', link: 'https://twitter.com/juunegreiros' },
+    { id: 456, name: 'rafaballerini', link: 'https://github.com/rafaballerini' },
+    { id: 567, name: 'marcobrunodev', link: 'https://www.instagram.com/marcobrunodev/' },
+    { id: 789, name: 'felipefialho', link: 'https://github.com/felipefialho' },
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dadosForm = new FormData(event.target);
+
+    const comunidade = {
+      id: new Date().getTime(),
+      name: dadosForm.get('title'),
+      image: dadosForm.get('image') ? dadosForm.get('image') : 'https://placehold.it/300x300'
+    }
+    return setComunidades([...comunidades, comunidade])
+  }
 
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={githubUser} />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <ProfileSidebar gitUser={gitUser} />
+          <ProfileSidebar githubUser={githubUser} />
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
@@ -36,28 +86,39 @@ export default function Home() {
               Bem Vindo(a)
             </h1>
 
-            <OrkutNostalgicIconSet />
+            <OrkutNostalgicIconSet recados={1} fotos={3} fas={10} mensagens={99} legal={3} confiavel={3} sexy={2} />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para ser usada de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para ser usada de capa"
+                  required
+                />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
         <div className="profileRelarionsArea" style={{ gridArea: 'profileRelarionsArea' }}>
-          <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-              Pessoas da Comunidade ({pessoasFavoritas.length})
-            </h2>
+          <GroupRenders array={comunidades} groupName={'Comunidades'} />
 
-            <ul>
-              {pessoasFavoritas.map(pessoas => {
-                return (
-                  <li key={pessoas}>
-                    <a href={`/users/${pessoas}`}>
-                    <img src={`https://github.com/${pessoas}.png`} />
-                    <span>{pessoas}</span>
-                  </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </ProfileRelationsBoxWrapper>
+          <GroupRenders array={pessoasFavoritas} groupName={'Pessoas Favoritas'} />
         </div>
       </MainGrid>
     </>
--- src/lib/AluraCommons.js
+++ src/lib/AluraCommons.js
@@ -54,7 +54,7 @@ export function AlurakutMenu({ githubUser }) {
 }
 AlurakutMenu.Wrapper = styled.header`
   width: 100%;
-  background-color: #308BC5;
+  background-color: #7f8c8d;
   .alurakutMenuProfileSidebar {
     background: white;
     position: fixed;
@@ -80,7 +80,7 @@ AlurakutMenu.Wrapper = styled.header`
     }
     .boxLink {
       font-size: 18px;
-      color: #2E7BB4;
+      color: #636e72;
       -webkit-text-decoration: none;
       text-decoration: none;
       font-weight: 800;
@@ -93,7 +93,7 @@ AlurakutMenu.Wrapper = styled.header`
     }
   }
   .container {
-    background-color: #308BC5;
+    background-color: #7f8c8d;
     padding: 7px 16px;
     max-width: 1110px;
     margin: auto;
@@ -126,7 +126,7 @@ AlurakutMenu.Wrapper = styled.header`
         text-decoration: none;
         &:after {
           content: " ";
-          background-color: #5292C1;
+          background-color: #b2bec3;
           display: block;
           position: absolute;
           width: 1px;
@@ -140,7 +140,7 @@ AlurakutMenu.Wrapper = styled.header`
     }
     input {
       color: #ffffff;
-      background: #5579A1;
+      background: #95a5a6;
       padding: 10px 42px;
       border: 0;
       background-image: url(${`${BASE_URL}/icons/search.svg`});
@@ -156,7 +156,7 @@ AlurakutMenu.Wrapper = styled.header`
   }
 `;
 AlurakutMenu.Logo = styled.img`
-  background-color: #ffffff;
+  background-color: #ecf0f1;
   padding: 9px 14px;
   border-radius: 1000px;
   height: 34px;
@@ -222,7 +222,7 @@ export function AlurakutProfileSidebarMenuDefault() {
 AlurakutProfileSidebarMenuDefault.Wrapper = styled.div`
   a {
     font-size: 12px;
-    color: #2E7BB4;
+    color: #7f8c8d;
     margin-bottom: 16px;
     display: flex;
     align-items: center;
@@ -264,7 +264,7 @@ export function OrkutNostalgicIconSet(props) {
         { name: 'Legal', slug: 'legal', icon: 'cool' },
         { name: 'Sexy', slug: 'sexy', icon: 'heart' },
       ].map(({ name, slug, icon }) => {
-        const total = props[slug] ? props[slug] : 2;
+        const total = props[slug] ? props[slug] : 1;
         return (
           <li key={`orkut__icon_set__${slug}`}>
             <span className="OrkutNostalgicIconSet__title">
